94f19c8f05b06df5ee91009383ba280d26243738
johhns <johhns@hotmail.com>
2016-02-12 16:05:48 -0600

    ultimo update

--- ordenaPizza/ViewController.swift
+++ ordenaPizza/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnOut(sender: AnyObject) {
+        exit(0)
+    }
 
 }
 
